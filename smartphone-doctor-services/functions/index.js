ecff51e47fcd16f45b9bae914afe16643589bde9
bilger-progress <bilger.yahov@progress.com>
2018-11-22 18:49:20 +0200

    Implement another testing cloud funcrtion.

--- smartphone-doctor-services/functions/index.js
+++ smartphone-doctor-services/functions/index.js
@@ -4,8 +4,13 @@ const functions = require('firebase-functions');
 
 // Functions imported.
 const helloWorld = require("./src/hello-world");
+const sayHi = require("./src/say-hi");
 
 // Function main handlers.
 exports.helloWorld = functions.https.onRequest((request, response) => {
     return helloWorld(request, response);
+});
+
+exports.sayHi = functions.https.onRequest((request, response) => {
+    return sayHi(request, response);
 });

b345d1f9a8de39728eeae3c7f3f5181ac94849bb
bilger-progress <bilger.yahov@progress.com>
2018-12-07 14:54:40 +0200

    Build issues.

--- smartphone-doctor-services/functions/src/admin-sample.js
+++ smartphone-doctor-services/functions/src/admin-sample.js
@@ -6,10 +6,10 @@ module.exports = (request, response) => {
     admin.auth().getUserByEmail("test@test.test")
         .then(function (userRecord) {
             console.log("Successfully fetched user data: ", userRecord.toJSON());
-            response.send("All Good!");
+            return response.send("All Good!");
         })
         .catch(function (error) {
             console.error("Error fetching user data: ", error);
-            response.send("Not Good!");
+            return response.send("Not Good!");
         });
 };

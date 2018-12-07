b9374b094ca9825ef255661fff3e90eab43ac1f4
bilger-progress <bilger.yahov@progress.com>
2018-12-07 15:28:13 +0200

    Still the same.

--- smartphone-doctor-services/functions/index.js
+++ smartphone-doctor-services/functions/index.js
@@ -8,18 +8,13 @@ const helloWorld = require("./src/hello-world");
 const sayHi = require("./src/say-hi");
 const adminSample = require("./src/admin-sample");
 
-// Prepare Firebase Admin SDK's private key.
-let firebaseAdminPrivateKey = functions.config().admin.private_key;
-firebaseAdminPrivateKey = firebaseAdminPrivateKey.replace(/\\n/g, '\n');
-firebaseAdminPrivateKey = firebaseAdminPrivateKey.replace(/\\u0020/g, '\u0020');
-
 // Initialize the Firebase Admin SDK.
 admin.initializeApp({
     credential: admin.credential.cert({
         type: functions.config().admin.type,
         project_id: functions.config().admin.project_id,
         private_key_id: functions.config().admin.private_key_id,
-        private_key: firebaseAdminPrivateKey,
+        private_key: JSON.parse(functions.config().admin.private_key),
         client_email: functions.config().admin.client_email,
         client_id: functions.config().admin.client_id,
         auth_uri: functions.config().admin.auth_uri,

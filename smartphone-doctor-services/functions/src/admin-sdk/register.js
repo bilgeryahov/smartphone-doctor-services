8109f89cab954c220f4ae176bda29db9ded07ccf
bilger-progress <bilger.yahov@progress.com>
2018-12-22 20:18:25 +0200

    Re-work the "register.js", so it implements the new style of moduling.

--- smartphone-doctor-services/functions/src/admin-sdk/register.js
+++ smartphone-doctor-services/functions/src/admin-sdk/register.js
@@ -5,36 +5,38 @@
  * platform.
  * 
  * @author Bilger Yahov
- * @version 1.0.0
- * @copyright © 2018 Bilger Yahov, all rights reserved.
+ * @version 1.1.0
+ * @copyright © 2018 - 2019 Bilger Yahov, all rights reserved.
  */
 
 "use strict"
 
 const admin = require('firebase-admin');
-const cors = require('cors')({
-    origin: true,
-});
+
+const responseBuilder = require("../common-js/response-builder");
+const messageCodes = require("../common-js/message-codes");
+const statusCodes = require("../common-js/status-codes");
 
 /**
  * Goes through a set of checks before attempting to create an account.
  * All request methods different from POST are disallowed.
  * There's an input validation, which makes sure that the following three
  * body parameters are present and valid:
- *  -> e-mail address { string }
- *  -> password { string }
- *  -> isAgent { boolean }
+ *  - e-mail address { String }
+ *  - password { String }
+ *  - isAgent { Boolean }
  */
-module.exports = (request, response) => {
-    // Allow Cross-Origin-Resource-Sharing.
-    return cors(request, response, () => {
+module.exports = (request) => {
+    return new Promise((resolve, reject) => {
         // Allow only POST requests.
         if (request.method !== 'POST') {
-            return response.status(405).json({ error: 'HTTP Method is not allowed!' });
+            return reject(new responseBuilder.Response("The used HTTP method is not allowed!", messageCodes.httpRequest.failure,
+                statusCodes.MethodNotAllowed));
         }
         // Input validation.
         if (!inputCheck(request)) {
-            return response.status(400).json({ error: 'Badly formatted input values!' });
+            return reject(new responseBuilder.Response("Badly formatted input values!", messageCodes.httpRequest.failure,
+                statusCodes.BadRequest));
         }
         // If the new user is an agent, then make sure to disable initially.
         const initialUserData = {
@@ -50,20 +52,17 @@ module.exports = (request, response) => {
             disabled: initialUserData.disabled
         })
             .then(function (userRecord) {
-                console.log("#register.js: Successfully created new user: ", userRecord.uid);
-                return response.status(201).json({ message: { isAgent: request.body.isAgent, log: "Account created!" } });
+                console.log("#register.js: Successfully created new user: " + userRecord.uid);
+                return resolve(new responseBuilder.Response({ info: "Account created!", isAgent: request.body.isAgent },
+                    messageCodes.auth.success, statusCodes.Created));
             })
             .catch(function (error) {
-                console.log("#register.js: Error creating new user: ", JSON.stringify(error));
                 // TODO: Not the best way of doing it, since the error message
                 // that comes from the Admin SDK can contain sensitive information.
-                if (error.code) {
-                    return response.status(500).json({
-                        error: "Failed to create a new user!",
-                        code: error.code
-                    });
-                }
-                return response.status(500).json({ error: "Failed to create a new user!" });
+                console.error("#register.js: Error while creating new user: " + JSON.stringify(error));
+                let messageCode = error.code || messageCodes.auth.failure;
+                return reject(new responseBuilder.Response("Failed to create a new user!", messageCode,
+                    statusCodes.InternalServerError));
             });
     });
 };

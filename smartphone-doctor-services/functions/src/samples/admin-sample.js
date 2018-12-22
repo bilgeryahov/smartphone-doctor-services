a743cb2f90a2f66083be9f5f6b8de55613f1a76d
bilger-progress <bilger.yahov@progress.com>
2018-12-22 20:02:12 +0200

    Re-work the "admin-sample.js", so it implements the new style of moduling.

--- smartphone-doctor-services/functions/src/samples/admin-sample.js
+++ smartphone-doctor-services/functions/src/samples/admin-sample.js
@@ -5,11 +5,15 @@
  * Firebase Admin SDK.
  * 
  * @author Bilger Yahov
- * @version 1.0.0
- * @copyright © 2018 Bilger Yahov, all rights reserved.
+ * @version 2.0.0
+ * @copyright © 2018 - 2019 Bilger Yahov, all rights reserved.
  */
 
 "use strict"
+
+const responseBuilder = require("../common-js/response-builder");
+const messageCodes = require("../common-js/message-codes");
+const statusCodes = require("../common-js/status-codes");
 
 const admin = require('firebase-admin');
 
@@ -17,14 +21,19 @@ const admin = require('firebase-admin');
  * This is a test user, which is disabled on the Firebase console.
  * Will be used only to verify the set-up of Firebase Admin SDK.
  */
-module.exports = (request, response) => {
-    admin.auth().getUserByEmail("test@test.test")
-        .then(function (userRecord) {
-            console.log("#admin-sample.js: Successfully fetched user data: ", userRecord.toJSON());
-            return response.send("Firebase Admin SDK is up!");
-        })
-        .catch(function (error) {
-            console.error("#admin-sample.js: Error fetching user data: ", error);
-            return response.send("Problems with Firebase Admin SDK!");
-        });
+const TEST_USER = "test@test.test";
+
+module.exports = (request) => {
+    return new Promise((resolve, reject) => {
+        admin.auth().getUserByEmail(TEST_USER)
+            .then(function (userRecord) {
+                console.log("#admin-sample.js: Successfully fetched user data: " + JSON.stringify(userRecord));
+                return resolve(new responseBuilder.Response("Firebase Admin SDK is up!", messageCodes.samples.success, statusCodes.OK));
+            })
+            .catch(function (error) {
+                console.error("#admin-sample.js: Error while fetching user data: " + JSON.stringify(error));
+                return reject(new responseBuilder.Response("Firebase Admin SDK is down!", messageCodes.samples.failure,
+                    statusCodes.InternalServerError));
+            });
+    });
 };

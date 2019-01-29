29756aee0ec598556c4cae272286a6f15aec1bde
bilger-progress <bilger.yahov@progress.com>
2019-01-29 21:47:36 +0200

    Add Firebase BaaS message codes.

---
+++ smartphone-doctor-services/functions/test/firebase-message-codes.js
@@ -0,0 +1,17 @@
+/**
+ * @file firebase-message-codes.js
+ *
+ * List of message codes that are returned from Firebase BaaS.
+ * 
+ * @author Bilger Yahov
+ * @version 1.0.0
+ * @copyright © 2019 Bilger Yahov, all rights reserved.
+ */
+
+"use strict"
+
+module.exports = {
+    AUTH: {
+        emailExists: "auth/email-already-exists"
+    }
+}

e6ce4d321356fb9dd11e27ed7523ffe321cafc29
bilger-progress <bilger.yahov@progress.com>
2019-01-29 21:44:37 +0200

    Add a global conf for the app/services.

---
+++ smartphone-doctor-services/functions/global_conf.js
@@ -0,0 +1,16 @@
+/**
+ * @file global-conf.js
+ *
+ * Global conf values/details for the services/app.
+ * 
+ * @author Bilger Yahov
+ * @version 1.0.0
+ * @copyright © 2019 Bilger Yahov, all rights reserved.
+ */
+
+"use strict"
+
+module.exports = {
+    TEST_USER: "smrtphnsrvcs@smrt.phnsrvcs",
+    LOCAL_SERVER: "http://localhost:5000/smartphone-doctor/us-central1"
+}

69a5d43050660c4702da4ca7621f1695e2a4f1ed
bilger-progress <bilger.yahov@progress.com>
2019-01-30 12:39:27 +0200

    Revert the last revert.

--- smartphone-doctor-services/functions/test/hello-world.test.js
+++ smartphone-doctor-services/functions/test/hello-world.test.js
@@ -2,9 +2,9 @@
 
 const assert = require('assert');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
+describe('Sample.', () => {
+    describe('Testing.', () => {
+        it('Should return -1 when the value is not present.', () => {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });

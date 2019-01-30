dc1f6f860015015d48108ce8f45dc2e7c67fcebd
bilger-progress <bilger.yahov@progress.com>
2019-01-30 16:48:39 +0200

    Docs.

--- smartphone-doctor-services/functions/warm-up-timer.js
+++ smartphone-doctor-services/functions/warm-up-timer.js
@@ -1,5 +1,12 @@
 "use strict"
 
+// In seconds
+const WARM_UP_TIME = 30;
+
+/**
+ * Warm-up for the Firebase Functions Emulator
+ * during the CI process.
+ */
 setTimeout(() => {
     process.exit(0);
-}, 30000);
+}, WARM_UP_TIME * 1000);
